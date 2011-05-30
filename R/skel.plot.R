89d5ded2b9a7d0d5adbb30375b61fb383c26f8e0
Mikko Korpela <mvkorpel@iki.fi>
2011-05-30 11:03:25 +0000

    dplR 1.1.1
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/dplR@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/skel.plot.R
+++ R/skel.plot.R
@@ -1,9 +1,13 @@
 `skel.plot` <-
-function(y)
+function(y, sname = '')
 {
-  op=par(no.readonly=TRUE) # Save par for resetting
+  if(nchar(sname) > 7) stop('sname must be a character vector less than 8 characters long')
+
+  # should wrap this into a function called skel.calc that returns the
+  # dates and z_rel
   y=y[!is.na(y)]
   yr.vec=1:length(y)
+
   if(!is.null(names(y))) yr.vec=as.numeric(names(y))
   y.dt=hanning(y,9)
 
@@ -22,16 +26,74 @@ function(y)
   z_rel[z_rel < 3]=NA
   z_rel=ceiling(z_rel)
 
-  mat=matrix(1:2,2,1)
-  layout(mat)
-  par(mar=c(3,3,3,3),mgp=c(1.25,0.25,0),tcl=0.25)
-  plot(yr.vec,y,type="l",xlab="Years",ylab="",
-       main="Original series with 9-wt Hanning filter")
-  abline(v=pretty(range(yr.vec),n=20),col="grey")
-  lines(yr.vec,y.dt,lwd=2,col="red")
-  plot(yr.vec,z_rel,type="h",lwd=2,xlab="Years",ylab="",ylim=c(0,10),
-       main="Skeleton plot on residuals")
-  abline(v=pretty(range(yr.vec),n=20),col="grey")
-  par(op) # Reset par
+  # plot width in mm
+  series.length <- length(yr.vec)*2
+  plot.width=series.length%/%10*10+10
+  plot.height=11*2
+
+  grid.newpage()
+  pushViewport(viewport(layout=grid.layout(nrow=1,ncol=1,
+    widths=unit(plot.width,"mm"),heights=unit(plot.height,"mm"))))
+  pushViewport(viewport(layout.pos.col = 1, layout.pos.row = 1))
+  # green grid
+
+  # seq for 0 to plot width by 2mm
+  grid.segments(x0=unit(seq(0,plot.width,by=2),'mm'),y0=unit(0,'mm'),
+                x1=unit(seq(0,plot.width,by=2),'mm'),y1=unit(plot.height,'mm'),
+                gp = gpar(col='green', lineend = 'square', linejoin = 'round'))
+  grid.segments(x0=unit(0,'mm'),y0=unit(seq(0,plot.height,by=2),'mm'),
+                x1=unit(plot.width,'mm'),y1=unit(seq(0,plot.height,by=2),'mm'),
+                gp = gpar(col='green', lineend = 'square', linejoin = 'round'))
+
+  # decadal lines
+  grid.segments(x0=unit(seq(0,plot.width,by=20),'mm'),y0=unit(0,'mm'),
+                x1=unit(seq(0,plot.width,by=20),'mm'),y1=unit(plot.height,'mm'),
+                gp = gpar(col = 'black',lwd = 1.5, lty = 'dashed',
+                lineend = 'square', linejoin = 'round'))
+
+  # start and finish arrows
+  grid.lines(x=unit(c(0,0),'mm'),y=unit(c(plot.height,0),'mm'),
+             gp = gpar(lwd = 2, lineend = 'square', linejoin = 'round'))
+  grid.polygon(x=unit(c(0, 0, -2), 'mm'),
+               y=unit(c(0, 6, 6), 'mm'),
+               gp=gpar(fill = 'black', lineend = 'square', linejoin = 'round'))
+  grid.lines(x=unit(c(series.length,series.length),'mm'),y=unit(c(plot.height,0),'mm'),
+             gp = gpar(lwd = 2, lineend = 'square', linejoin = 'round'))
+  grid.polygon(x=unit(c(series.length, series.length, series.length+2), 'mm'),
+               y=unit(c(0, 6, 6), 'mm'),
+               gp=gpar(fill = 'black', lineend = 'square', linejoin = 'round'))
+
+  # lines on top and bottom of plot
+  grid.lines(x=unit(c(0,plot.width),'mm'),
+             y=unit(c(plot.height,plot.height),'mm'),
+             gp=gpar(lwd = 2, lineend = 'square', linejoin = 'round'))
+  grid.lines(x=unit(c(0,plot.width),'mm'),
+             y=unit(c(0,0),'mm'),
+             gp=gpar(lwd = 2, lineend = 'square', linejoin = 'round'))
+  # plot x axis
+  ticks <- seq(0,plot.width/2,by=10)
+  for(i in 1:length(ticks)){
+    grid.text(label = as.character(ticks[i]),
+              x=unit(ticks[i]*2,'mm'),
+              y=unit(plot.height+1,'mm'),
+              just = c('center','bottom'),
+              gp = gpar(fontsize=10))
+  }
+  # plot sample id
+  grid.text(label = sname,
+            x=unit(-1,'mm'),
+            y=unit(plot.height,'mm'),
+            just = c('right','bottom'),
+            rot = 90,
+            gp = gpar(fontsize=10))
+
+  # plot data
+  for(i in 1:length(yr.vec)){
+    if(!is.na(z_rel[i])){
+      grid.lines(x=unit(c((i-1)*2,(i-1)*2),'mm'),y=unit(c(0,z_rel[i]*2),'mm'),
+        gp = gpar(col = 'black',lwd = 2, lineend = 'square',
+        linejoin = 'round'))
+    }
+  }
 }
 

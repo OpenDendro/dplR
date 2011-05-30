6d39c53313c8bb10dc4ceb9cd56a368ad15402b1
Mikko Korpela <mvkorpel@iki.fi>
2011-05-30 13:28:24 +0000

    gini.c and sens.c from dplR 1.4.1
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/dplR@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/gini.c
+++ src/gini.c
@@ -3,66 +3,65 @@
 
 /* Written by Mikko Korpela */
 void gini(double *x_const, int *n_ptr, double *result){
-  int i;
-  double *x;
-  long double sum1, sum2, this_x;
-  listnode *tmp1, *tmp2;
-  int n = *n_ptr;
+    int i;
+    double *x;
+    dplr_double sum1, sum2;
+    listnode tmp1, tmp2, *tmp_p;
+    int n = *n_ptr;
 
-  if(n<2){
-    *result = 0;
-    return;
-  }
-
-  /* Sort the numbers */
-  x = (double *) R_alloc(n, sizeof(double));
-  for(i=0;i<n;i++)
-    x[i] = x_const[i];
-  R_qsort(x, 1, n);
-
-  /* Setup for grow_exp */
-  tmp1 = (listnode *) R_alloc(1, sizeof(listnode));
-  tmp1->next = NULL;
-  tmp1->data = (long double)x[0];
-  tmp1->valid = 1;
-
-  /* Cumulative sum */
-  for(i=1;i<n;i++){
-    grow_exp(tmp1, (long double)x[i]);
-    tmp2 = tmp1;
-    sum1 = 0;
-    while(tmp2 && tmp2->valid){
-      sum1 += tmp2->data;
-      tmp2 = tmp2->next;
+    if(n < 2){
+	*result = 0.0f;
+	return;
     }
-    x[i] = (double)sum1;
-  }
 
-  /* Setup for grow_exp */
-  if(tmp1->next)
-    tmp1->next->valid = 0;
-  tmp2 = (listnode *) R_alloc(1, sizeof(listnode));
-  tmp2->next = NULL;
+    /* Sort the numbers */
+    x = (double *) R_alloc(n, sizeof(double));
+    for(i = 0; i < n; i++)
+	x[i] = x_const[i];
+    R_qsort(x, 1, n);
 
-  /* Gini */
-  tmp1->data = (long double)x[n-1] * (n-1);
-  tmp2->data = (long double)x[0];
-  tmp2->valid = 1;
-  grow_exp(tmp2, (long double)x[0]);
-  for(i=1;i<n-1;i++){
-    this_x = (long double)x[i];
-    grow_exp(tmp1, this_x * i);
-    grow_exp(tmp2, this_x * (i+2));
-  }
-  sum1 = 0;
-  while(tmp1 && tmp1->valid){
-    sum1 += tmp1->data;
-    tmp1 = tmp1->next;
-  }
-  sum2 = 0;
-  while(tmp2 && tmp2->valid){
-    sum2 += tmp2->data;
-    tmp2 = tmp2->next;
-  }
-  *result = (double)((sum1-sum2)/((long double)x[n-1]*n));
+    /* Setup for grow_exp */
+    tmp1.next = NULL;
+    tmp1.data = x[0];
+    tmp1.valid = 1;
+
+    /* Cumulative sum */
+    for(i = 1; i < n; i++){
+	grow_exp(&tmp1, x[i]);
+	tmp_p = &tmp1;
+	sum1 = 0.0f;
+	while(tmp_p && tmp_p->valid){
+	    sum1 += tmp_p->data;
+	    tmp_p = tmp_p->next;
+	}
+	x[i] = sum1;
+    }
+
+    /* Setup for grow_exp */
+    if(tmp1.next)
+	tmp1.next->valid = 0;
+    tmp2.next = NULL;
+
+    /* Gini */
+    tmp1.data = (dplr_double)x[n-1] * (n-1);
+    tmp2.data = x[0];
+    tmp2.valid = 1;
+    grow_exp(&tmp2, x[0]);
+    for(i = 1; i < n-1; i++){
+	grow_exp(&tmp1, (dplr_double)x[i] * i);
+	grow_exp(&tmp2, (dplr_double)x[i] * (i+2));
+    }
+    sum1 = 0.0f;
+    tmp_p = &tmp1;
+    while(tmp_p && tmp_p->valid){
+	sum1 += tmp_p->data;
+	tmp_p = tmp_p->next;
+    }
+    sum2 = 0.0f;
+    tmp_p = &tmp2;
+    while(tmp_p && tmp_p->valid){
+	sum2 += tmp_p->data;
+	tmp_p = tmp_p->next;
+    }
+    *result = (sum1-sum2)/((dplr_double)x[n-1]*n);
 }
--- src/sens.c
+++ src/sens.c
@@ -3,107 +3,106 @@
 
 /* Written by Mikko Korpela */
 void sens2(double *x_const, int *n_ptr, double *result){
-  int i;
-  double previous, this, next;
-  long double sum1, sum2;
-  listnode *tmp, *expansion;
-  int n = *n_ptr;
+    int i;
+    double previous, this, next;
+    dplr_double sum1, sum2;
+    listnode tmp, *tmp_p;
+    int n = *n_ptr;
 
-  if(n<2){
-    *result = R_NaN;
-    return;
-  }
-
-  /* Setup for grow_exp and msum */
-  tmp = (listnode *) R_alloc(1, sizeof(listnode));
-  tmp->next = NULL;
-  tmp->valid = 0;
-
-  /* In the sum of absolute differences between consecutive elements of
-     an array, each number will appear multiplied by -2, -1, 0, 1, or 2
-     (first and last number by -1, 0, or 1) */
-  this = x_const[0];
-  next = x_const[1];
-  if(this > next){
-    grow_exp(tmp, (long double)this);
-  } else if(this < next){
-    grow_exp(tmp, (long double)-this);
-  }
-  for(i=1;i<n-1;i++){
-    previous = x_const[i-1];
-    this = x_const[i];
-    next = x_const[i+1];
-    if(this > previous){
-      if(this > next){
-	grow_exp(tmp, (long double)this);
-	grow_exp(tmp, (long double)this);
-      } else if(this == next){
-	grow_exp(tmp, (long double)this);
-      }
-    } else if(this < previous){
-      if(this < next){
-	grow_exp(tmp, (long double)-this);
-	grow_exp(tmp, (long double)-this);
-      } else if(this == next){
-	grow_exp(tmp, (long double)-this);
-      }
-    } else if(this > next){
-      grow_exp(tmp, (long double)this);
-    } else if(this < next){
-      grow_exp(tmp, (long double)-this);
+    if(n < 2){
+	*result = R_NaN;
+	return;
     }
-  }
-  this = x_const[n-1];
-  previous = x_const[n-2];
-  if(this > previous){
-    grow_exp(tmp, (long double)this);
-  } else if(this < previous){
-    grow_exp(tmp, (long double)-this);
-  }
 
-  /* Sum of absolute differences */
-  sum1 = 0;
-  expansion = tmp;
-  while(expansion && expansion->valid){
-    sum1 += expansion->data;
-    expansion = expansion->next;
-  }
+    /* Setup for grow_exp and msum */
+    tmp.next = NULL;
+    tmp.valid = 0;
+    tmp_p = &tmp;
 
-  sum2 = msum(x_const, n, tmp);
-  *result = (double)(sum1/(sum2-sum2/n));
+    /* In the sum of absolute differences between consecutive elements
+       of an array, each number will appear multiplied by -2, -1, 0,
+       1, or 2 (first and last number by -1, 0, or 1) */
+    this = x_const[0];
+    next = x_const[1];
+    if(this > next){
+	grow_exp(tmp_p, this);
+    } else if(this < next){
+	grow_exp(tmp_p, -this);
+    }
+    for(i = 1; i < n-1; i++){
+	previous = x_const[i-1];
+	this = x_const[i];
+	next = x_const[i+1];
+	if(this > previous){
+	    if(this > next){
+		grow_exp(tmp_p, this);
+		grow_exp(tmp_p, this);
+	    } else if(this == next){
+		grow_exp(tmp_p, this);
+	    }
+	} else if(this < previous){
+	    if(this < next){
+		grow_exp(tmp_p, -this);
+		grow_exp(tmp_p, -this);
+	    } else if(this == next){
+		grow_exp(tmp_p, -this);
+	    }
+	} else if(this > next){
+	    grow_exp(tmp_p, this);
+	} else if(this < next){
+	    grow_exp(tmp_p, -this);
+	}
+    }
+    this = x_const[n-1];
+    previous = x_const[n-2];
+    if(this > previous){
+	grow_exp(tmp_p, this);
+    } else if(this < previous){
+	grow_exp(tmp_p, -this);
+    }
+
+    /* Sum of absolute differences */
+    sum1 = 0.0f;
+    while(tmp_p && tmp_p->valid){
+	sum1 += tmp_p->data;
+	tmp_p = tmp_p->next;
+    }
+
+    sum2 = msum(x_const, n, &tmp);
+    *result = sum1/(sum2-sum2/n);
 }
 
 /* Written by Mikko Korpela */
 void sens1(double *x_const, int *n_ptr, double *result){
-  int i;
-  long double previous, this, sum, term;
-  listnode *tmp;
-  int n = *n_ptr;
+    int i;
+    dplr_double sum, previous, this, term;
+    listnode tmp, *tmp_p;
+    int n = *n_ptr;
 
-  if(n<2){
-    *result = R_NaN;
-    return;
-  }
+    if(n < 2){
+	*result = R_NaN;
+	return;
+    }
 
-  /* Setup for grow_exp */
-  tmp = (listnode *) R_alloc(1, sizeof(listnode));
-  tmp->next = NULL;
-  tmp->valid = 0;
+    /* Setup for grow_exp */
+    tmp.next = NULL;
+    tmp.valid = 0;
+    tmp_p = &tmp;
 
-  for(i=1;i<n;i++){
-    previous = (long double)x_const[i-1];
-    this = (long double)x_const[i];
-    term = (this>previous?this-previous:previous-this)/(this+previous);
-    if(!ISNAN(term))
-      grow_exp(tmp, term);
-  }
+    for(i = 1; i < n; i++){
+	previous = x_const[i-1];
+	this = x_const[i];
+	term = (this>previous?this-previous:previous-this)/(this+previous);
+	if(!ISNAN(((double)term)))
+	    grow_exp(tmp_p, term);
+    }
 
-  /* Sum of scaled absolute differences */
-  sum = 0;
-  while(tmp && tmp->valid){
-    sum += tmp->data;
-    tmp = tmp->next;
-  }
+    /* Sum of scaled absolute differences */
+    sum = 0.0f;
+    while(tmp_p && tmp_p->valid){
+	sum += tmp_p->data;
+	tmp_p = tmp_p->next;
+    }
 
-  *result = (double)((sum+sum)/(n-1));
+    *result = (sum+sum)/(n-1);
 }

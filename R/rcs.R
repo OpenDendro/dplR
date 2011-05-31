61a607a5735166fdaf2adf4e5b0f2257e884016b
Mikko Korpela <mvkorpel@iki.fi>
2011-05-31 16:42:09 +0000

    Some values are stored for reuse
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rcs.R
+++ R/rcs.R
@@ -1,8 +1,10 @@
 rcs <- function(rwl, po, nyrs=NULL, f=0.5, biweight=TRUE, rc.out=FALSE,
                 make.plot=TRUE, ...) {
-    if(ncol(rwl) != nrow(po))
+    n.col <- ncol(rwl)
+    if(n.col != nrow(po))
         stop("dimension problem: ncol(rw) != nrow(po)")
-    if(!all(po[, 1] %in% colnames(rwl)))
+    col.names <- colnames(rwl)
+    if(!all(po[, 1] %in% col.names))
         stop("series ids in 'po' and 'rwl' do not match")
     if(any(po[, 2] < 1))
         stop("minimum 'po' is 1")
@@ -12,21 +14,21 @@ rcs <- function(rwl, po, nyrs=NULL, f=0.5, biweight=TRUE, rc.out=FALSE,
     series.yrs <- apply(rwl, 2, yr.range)
     rownames(series.yrs) <- c("first", "last")
 
-    rwl.ord<-apply(rwl, 2, sortByIndex)
+    rwl.ord <- apply(rwl, 2, sortByIndex)
     rwca <- data.frame(matrix(NA,
-                              ncol=ncol(rwl.ord),
+                              ncol=n.col,
                               nrow=sum(nrow(rwl.ord) + max(po[, 2]))))
-    colnames(rwca) <- colnames(rwl)
-    for (i in 1:ncol(rwl.ord)){
-        series <- colnames(rwl.ord)[i]
-        yrs2pith <- po[po[, 1] %in% series, 2]
-        rwca[(yrs2pith):(yrs2pith + nrow(rwl.ord)-1), i]<-rwl.ord[, i]
+    colnames(rwca) <- col.names
+    nrow.m1 <- nrow(rwl.ord) - 1
+    for (i in 1:n.col){
+        yrs2pith <- po[po[, 1] %in% col.names[i], 2]
+        rwca[yrs2pith:(yrs2pith + nrow.m1), i] <- rwl.ord[, i]
     }
 
     if(biweight) ca.m <- apply(rwca, 1, tbrm, C = 9)
     else ca.m <- rowMeans(rwca, na.rm=TRUE)
 
-    ## spline follows B&Q 2008 as 10%of the RC length
+    ## spline follows B&Q 2008 as 10% of the RC length
     if(is.null(nyrs)) nyrs <- floor(length(na.omit(ca.m)) * 0.1)
     tmp <- ffcsaps(y=na.omit(ca.m), nyrs=nyrs, f=f)
     rc <- rep(NA, nrow(rwca))
@@ -36,7 +38,7 @@ rcs <- function(rwl, po, nyrs=NULL, f=0.5, biweight=TRUE, rc.out=FALSE,
     ## and restore to cal years
     rwi <- rwl
     yrs <- as.numeric(rownames(rwi))
-    for(i in 1:ncol(rwca)){
+    for(i in 1:n.col){
         first <- series.yrs[1, i]
         last <- series.yrs[2, i]
         ## check
@@ -50,7 +52,7 @@ rcs <- function(rwl, po, nyrs=NULL, f=0.5, biweight=TRUE, rc.out=FALSE,
         par(mar = c(4, 4, 4, 4) + 0.1, mgp = c(1.25, 0.25, 0), tcl = 0.25)
         plot(rwca[, 1], ylim=range(rwca, na.rm=T), type="n", ylab="mm",
              xlab="Cambial Age (Years)", ...)
-        for(i in 1:ncol(rwca)) lines(rwca[, i], col="grey")
+        for(i in 1:n.col) lines(rwca[, i], col="grey")
         lines(ca.m, lwd=1.5, col="black")
         lines(rc, lwd=2, col="red")
     }

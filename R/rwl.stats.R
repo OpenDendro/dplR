1520ba72a4de76b3c1dce5c022bf9f00bb2e1312
Mikko Korpela <mvkorpel@iki.fi>
2011-05-31 12:38:21 +0000

    - Removed redundant instances of c() - Cosmetic improvement
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@111 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rwl.stats.R
+++ R/rwl.stats.R
@@ -1,31 +1,30 @@
 `rwl.stats` <-
-function(rwl)
+    function(rwl)
 {
-  acf1=function(x){
-    x=x[!is.na(x)]
-    ar1=acf(x,lag.max=1,plot=FALSE)
-    ar1$acf[2]
-  }
-  skew=function(x){
-    x=x[!is.na(x)]
-    sum((x-mean(x))^3)/(length(x)*sd(x)^3)
-  }
+    acf1 <- function(x){
+        x <- x[!is.na(x)]
+        ar1 <- acf(x, lag.max=1, plot=FALSE)
+        ar1$acf[2]
+    }
+    skew <- function(x){
+        x <- x[!is.na(x)]
+        sum((x-mean(x))^3) / (length(x)*sd(x)^3)
+    }
 
-  series.stats=data.frame(series=colnames(rwl))
-  the.range = apply(rwl,2,yr.range)
-  series.stats$first=the.range[1,]
-  series.stats$last=the.range[2,]
-  series.stats$year=series.stats$last-series.stats$first+1
-  series.stats$mean=colMeans(rwl,na.rm=TRUE)
-  series.stats$median=apply(rwl,2,median,na.rm=TRUE)
-  series.stats$stdev=apply(rwl,2,sd,na.rm=TRUE)
-  series.stats$skew=apply(rwl,2,skew)
-  series.stats$sens1=apply(rwl,2,sens1)
-  series.stats$sens2=apply(rwl,2,sens2)
-  series.stats$gini=apply(rwl,2,gini.coef)
-  series.stats$ar1=apply(rwl,2,acf1)
-  series.stats[,-c(1:4)]=round(series.stats[,-c(1:4)],3)
+    series.stats <- data.frame(series=colnames(rwl))
+    the.range <- apply(rwl, 2, yr.range)
+    series.stats$first <- the.range[1, ]
+    series.stats$last <- the.range[2, ]
+    series.stats$year <- series.stats$last - series.stats$first + 1
+    series.stats$mean <- colMeans(rwl, na.rm=TRUE)
+    series.stats$median <- apply(rwl, 2, median, na.rm=TRUE)
+    series.stats$stdev <- apply(rwl, 2, sd, na.rm=TRUE)
+    series.stats$skew <- apply(rwl, 2, skew)
+    series.stats$sens1 <- apply(rwl, 2, sens1)
+    series.stats$sens2 <- apply(rwl, 2, sens2)
+    series.stats$gini <- apply(rwl, 2, gini.coef)
+    series.stats$ar1 <- apply(rwl, 2, acf1)
+    series.stats[, -(1:4)] <- round(series.stats[, -(1:4)], 3)
 
-  series.stats
+    series.stats
 }
-

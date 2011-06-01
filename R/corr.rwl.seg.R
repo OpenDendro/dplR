453bb1ebb6703831cfb71300f7eccebd83af0f0e
Mikko Korpela <mvkorpel@iki.fi>
2011-06-01 14:24:51 +0000

    - par(op) moved to on.exit() - Odd and even segs are plotted with the same code, avoiding copy-paste - Some values are stored for reuse - Some unnecessary colnames are not set - Cosmetic improvement
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/corr.rwl.seg.R
+++ R/corr.rwl.seg.R
@@ -2,14 +2,11 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
                          prewhiten = TRUE, pcrit=0.05, biweight=TRUE,
                          make.plot = TRUE, label.cex=1, ...){
 
-
     ## helper function
     yr.range <- function(x) {
         if(any(mask <- !is.na(x))) range(as.numeric(names(x))[mask])
         else c(NA, NA)
     }
-
-
 
     ## run error checks
     qa.xdate(rwl, seg.length, n, bin.floor)
@@ -22,7 +19,7 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
 
     rnames <- rownames(rwl)
     cnames <- colnames(rwl)
-    seg.lag <- seg.length/2
+    seg.lag <- seg.length / 2
     nseries <- ncol(rwl)
     if(nseries < 2) stop("At least 2 series are needed in rwl")
     yrs <- as.numeric(rnames)
@@ -30,7 +27,7 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
     min.yr <- min(yrs)
     max.yr <- max(yrs)
     if(is.null(bin.floor) || bin.floor == 0) min.bin <- min.yr
-    else min.bin <- ceiling(min.yr/bin.floor)*bin.floor
+    else min.bin <- ceiling(min.yr/bin.floor) * bin.floor
     bins <- seq(from=min.bin, to=max.yr-seg.length+1, by=seg.lag)
     bins <- cbind(bins, bins+(seg.length-1))
     nbins <- nrow(bins)
@@ -94,7 +91,8 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
             res.pval[i, j] <- bin.pval
         }
         ## overall correlation
-        tmp <- cor.test(series, master, method = "spearman", alternative = "g")
+        tmp <- cor.test(series, master,
+                        method = "spearman", alternative = "g")
         overall.cor[i, 1] <- tmp$estimate
         overall.cor[i, 2] <- tmp$p.val
     }
@@ -102,191 +100,120 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
     segavg.cor <- colMeans(res.cor, na.rm=T)
 
     ## make a list of problem segments
-    seg.flags <- rep(NA, nrow(res.pval))
-    names(seg.flags) <- rownames(res.pval)
+    seg.flags <- rep(NA, nseries)
+    names(seg.flags) <- cnames
     flag.logical <- res.pval >= pcrit
     flag.logical[is.na(flag.logical)] <- FALSE
     for(i in 1:length(seg.flags))
         seg.flags[i] <- paste(names(flag.logical[i, flag.logical[i, ]]),
                               collapse = ", ")
     seg.flags <- seg.flags[seg.flags != ""]
+
     ## plot
     if(make.plot){
         p.val <- res.pval
-        ## odd segs
         segs <- rwi
-        this.seq <- seq(from=1, to=nrow(bins), by=2)
-        odd.bins <- bins[this.seq, , drop=FALSE]
-        odd.p.val <- p.val[, this.seq, drop=FALSE]
-        nodd.bins <- nrow(odd.bins)
-        com.segs <- as.data.frame(matrix(1, ncol=nseries, nrow=nyrs))
-        rownames(com.segs) <- rnames
-        colnames(com.segs) <- cnames
-        flag.segs <- as.data.frame(matrix(NA, ncol=nseries, nrow=nyrs))
-        rownames(flag.segs) <- rnames
-        colnames(flag.segs) <- cnames
-        ## loop through odd.bins
-        tmp <- odd.p.val > pcrit
-        for(i in 1:nseries){
-            for(j in 1:nodd.bins){
-                ## minus 1 deals with edge in segment graphing
-                mask <- yrs%in%seq(from=odd.bins[j, 1], to=odd.bins[j, 2]-1)
-                mask2 <- yrs%in%seq(from=odd.bins[j, 1], to=odd.bins[j, 2])
-                ## note lack of complete overlap
-                if(any(is.na(segs[mask, i]))) com.segs[mask, i] <- NA
-                if(is.na(tmp[i, j])) com.segs[mask, i] <- NA
-                if(!is.na(tmp[i, j]) && tmp[i, j]) flag.segs[mask2, i] <- 1
-            }
-            ## make sure any segment past year range is NA for
-            ## com.segs this is a fix because there was an errant 1 in
-            ## floating series that was not getting tagged because of
-            ## the mask indexing: odd.bins[j, 2]-1 that deals with the
-            ## bin overlap issue. This should fix it.
-            series.yrs <- yr.range(segs[, i])
-            mask3 <- yrs%in%seq(series.yrs[1], series.yrs[2])
-            com.segs[!mask3, i] <- NA
-        }
-        idx.small <- yrs < min(odd.bins)
-        idx.large <- yrs > max(odd.bins)
-        com.segs[idx.small, ] <- NA
-        com.segs[idx.large, ] <- NA
-        flag.segs[idx.small, ] <- NA
-        flag.segs[idx.large, ] <- NA
-
         extreme.year <- apply(segs, 2, yr.range)
         first.year <- extreme.year[1, ]
         rsult <- sort.int(first.year, decreasing=FALSE, index.return=TRUE)
         neworder <- rsult$ix
         segs <- segs[, neworder, drop=FALSE]
-        com.segs <- com.segs[, neworder, drop=FALSE]
-        flag.segs <- flag.segs[, neworder, drop=FALSE]
-
-
-
-
         segs.df <- data.frame(t(extreme.year[, neworder]))
         names(segs.df) <- c("first.yr", "last.yr")
-        com.segs.df <- data.frame(t(apply(com.segs, 2, yr.range)))
-        names(com.segs.df) <- c("first.yr", "last.yr")
-        flag.segs.df <- data.frame(t(apply(flag.segs, 2, yr.range)))
-        names(flag.segs.df) <- c("first.yr", "last.yr")
+        first.year <- first.year[neworder]
+        last.year <- extreme.year[2, neworder]
 
+        nsegs <- ncol(segs)
         op <- par(no.readonly=TRUE)
+        on.exit(par(op), add=TRUE)
         col.pal <- c("#E41A1C", "#377EB8", "#4DAF4A")
         par(mar=c(4, 5, 4, 5) + 0.1, mgp=c(1.25, 0.25, 0), tcl=0.25)
-        plot(yrs, segs[, 1], type="n", ylim=c(0, ncol(segs)),
+        plot(yrs, segs[, 1], type="n", ylim=c(0, nsegs),
              axes=FALSE, ylab="", xlab="Year",
              sub=paste("Segments: length=", seg.length, ",lag=",
              seg.lag, sep=""), ...)
         ## bounding poly for even series
+        xx <- c(min.yr-100, max.yr+100)
+        xx <- c(xx, rev(xx))
         for(i in seq(from=1, to=nseries, by=2)){
-            xx <- c(min.yr-100, max.yr+100)
-            xx <- c(xx, rev(xx))
             yy <- c(i-0.5, i-0.5, i+0.5, i+0.5)
             polygon(xx, yy, col="grey90", border=NA)
         }
         abline(v=bins, col="grey", lty="dotted")
-        axis(1, at=odd.bins)
-        ## polygons for odd bins (go down from series line)
-        for(i in seq(from=1, to=nseries)){
-            y.deviation <- i-0.25
-            ## whole segs
-            xx <- c(segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            yy <- c(i, i, y.deviation, y.deviation)
-            polygon(xx, yy, col=col.pal[3], border=NA)
-            ## complete segs
-            xx <- c(com.segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            polygon(xx, yy, col=col.pal[2], border=NA)
-            ## flags
-            xx <- c(flag.segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            polygon(xx, yy, col=col.pal[1], border=NA)
-            ## guides
-            guides.x <- c(odd.bins, recursive=T)
-            guides.x <- sort(guides.x[!duplicated(guides.x)])
-            guides.x <- guides.x[guides.x >= segs.df[i, 1]]
-            guides.x <- guides.x[guides.x <= segs.df[i, 2]]
-            segments(guides.x, i, guides.x, y.deviation, col="white")
-        }
 
-################################################################################
-        ## even segs
-        this.seq <- seq(from=2, to=nrow(bins), by=2)
-        even.bins <- bins[this.seq, , drop=FALSE]
-        even.p.val <- p.val[neworder, this.seq, drop=FALSE]
-        neven.bins <- nrow(even.bins)
-###    com.segs[,]=1
-###    flag.segs[,]=NA
-        com.segs <- as.data.frame(matrix(1, ncol=nseries, nrow=nyrs))
-        rownames(com.segs) <- rnames
-        colnames(com.segs) <- cnames
-        flag.segs <- as.data.frame(matrix(NA, ncol=nseries, nrow=nyrs))
-        rownames(flag.segs) <- rnames
-        colnames(flag.segs) <- cnames
-
-        ## loop through even.bins
-        tmp <- even.p.val > pcrit
-        for(i in 1:nseries){
-            for(j in 1:neven.bins){
-                mask <- yrs%in%seq(from=even.bins[j, 1], to=even.bins[j, 2]-1)
-                mask2 <- yrs%in%seq(from=even.bins[j, 1], to=even.bins[j, 2])
-                ## note lack of complete overlap
-                if(any(is.na(segs[mask, i]))) com.segs[mask, i] <- NA
-                if(is.na(tmp[i, j])) com.segs[mask, i] <- NA
-                if(!is.na(tmp[i, j]) && tmp[i, j]) flag.segs[mask2, i] <- 1
+        ## First odd segs, then even segs
+        y.offset <- c(-0.25, 0.25)
+        ax <- c(1, 3)
+        for(odd.even in 1:2){
+            this.seq <- seq(from=odd.even, to=nbins, by=2)
+            these.bins <- bins[this.seq, , drop=FALSE]
+            com.segs <- as.data.frame(matrix(1, ncol=nseries, nrow=nyrs))
+            rownames(com.segs) <- rnames
+            flag.segs <- as.data.frame(matrix(NA, ncol=nseries, nrow=nyrs))
+            rownames(flag.segs) <- rnames
+            ## loop through these.bins
+            tmp <- p.val[neworder, this.seq, drop=FALSE] > pcrit
+            for(i in 1:nseries){
+                for(j in 1:nrow(these.bins)){
+                    ## minus 1 deals with edge in segment graphing
+                    mask <- yrs%in%seq(from = these.bins[j, 1],
+                                       to = these.bins[j, 2]-1)
+                    ## note lack of complete overlap
+                    if(any(is.na(segs[mask, i])))
+                        com.segs[mask, i] <- NA
+                    if(is.na(tmp[i, j]))
+                        com.segs[mask, i] <- NA
+                    else if(tmp[i, j]){
+                        mask2 <- yrs%in%seq(from = these.bins[j, 1],
+                                            to = these.bins[j, 2])
+                        flag.segs[mask2, i] <- 1
+                    }
+                }
+                ## make sure any segment past year range is NA for
+                ## com.segs this is a fix because there was an errant
+                ## 1 in floating series that was not getting tagged
+                ## because of the mask indexing: these.bins[j, 2]-1
+                ## that deals with the bin overlap issue. This should
+                ## fix it.
+                mask3 <- yrs%in%seq(first.year[i], last.year[i])
+                com.segs[!mask3, i] <- NA
             }
-            ## make sure any segment past year range is NA for
-            ## com.segs this is a fix because there was an errant 1 in
-            ## floating series that was not getting tagged because of
-            ## the mask indexing: odd.bins[j, 2]-1 that deals with the
-            ## bin overlap issue. This should fix it.
-            series.yrs <- yr.range(segs[, i])
-            mask3 <- yrs%in%seq(series.yrs[1], series.yrs[2])
-            com.segs[!mask3, i] <- NA
-        }
-        idx.small <- yrs < min(even.bins)
-        idx.large <- yrs > max(even.bins)
-        com.segs[idx.small, ] <- NA
-        com.segs[idx.large, ] <- NA
-        flag.segs[idx.small, ] <- NA
-        flag.segs[idx.large, ] <- NA
+            idx.small.large <-
+                yrs < min(these.bins) | yrs > max(these.bins)
+            com.segs[idx.small.large, ] <- NA
+            flag.segs[idx.small.large, ] <- NA
 
-        com.segs.df <- data.frame(t(apply(com.segs, 2, yr.range)))
-        names(com.segs.df) <- c("first.yr", "last.yr")
-        flag.segs.df <- data.frame(t(apply(flag.segs, 2, yr.range)))
-        names(flag.segs.df) <- c("first.yr", "last.yr")
+            com.segs.df <- data.frame(t(apply(com.segs, 2, yr.range)))
+            flag.segs.df <- data.frame(t(apply(flag.segs, 2, yr.range)))
 
-        ## guidelines
-        axis(3, at=even.bins)
-        ## polygons for even bins (go up from series line)
-        for(i in seq(from=1, to=nseries)){
-            y.deviation <- i+0.25
-            ## whole segs
-            xx <- c(segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            yy <- c(i, i, y.deviation, y.deviation)
-            polygon(xx, yy, col=col.pal[3], border=NA)
-            ## complete segs
-            xx <- c(com.segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            polygon(xx, yy, col=col.pal[2], border=NA)
-            ## flags
-            xx <- c(flag.segs.df[i, ], recursive=TRUE)
-            xx <- c(xx, rev(xx))
-            polygon(xx, yy, col=col.pal[1], border=NA)
-            ## guides
-            guides.x <- c(even.bins, recursive=T)
-            guides.x <- sort(guides.x[!duplicated(guides.x)])
-            guides.x <- guides.x[guides.x >= segs.df[i, 1]]
-            guides.x <- guides.x[guides.x <= segs.df[i, 2]]
-            segments(guides.x, i, guides.x, y.deviation, col="white")
-
+            axis(ax[odd.even], at=these.bins)
+            ## polygons for these bins (go down or up from series line)
+            y.deviation <- y.offset[odd.even]
+            guides.x.base <- c(these.bins, recursive=T)
+            guides.x.base <- sort(guides.x.base[!duplicated(guides.x.base)])
+            for(i in seq(from=1, to=nseries)){
+                y.deviation <- y.deviation + 1
+                ## whole segs
+                xx <- c(segs.df[i, ], recursive=TRUE)
+                xx <- c(xx, rev(xx))
+                yy <- c(i, i, y.deviation, y.deviation)
+                polygon(xx, yy, col=col.pal[3], border=NA)
+                ## complete segs
+                xx <- c(com.segs.df[i, ], recursive=TRUE)
+                xx <- c(xx, rev(xx))
+                polygon(xx, yy, col=col.pal[2], border=NA)
+                ## flags
+                xx <- c(flag.segs.df[i, ], recursive=TRUE)
+                xx <- c(xx, rev(xx))
+                polygon(xx, yy, col=col.pal[1], border=NA)
+                ## guides
+                guides.x <- guides.x.base[guides.x.base >= segs.df[i, 1]]
+                guides.x <- guides.x[guides.x <= segs.df[i, 2]]
+                segments(guides.x, i, guides.x, y.deviation, col="white")
+            }
         }
 
         ## finish up plotting
-        nsegs <- ncol(segs)
         odd.seq <- seq(from=1, to=nsegs, by=2)
         even.seq <- seq(from=2, to=nsegs, by=2)
         cnames.segs <- colnames(segs)
@@ -298,10 +225,8 @@ corr.rwl.seg <- function(rwl, seg.length=50, bin.floor=100, n=NULL,
              tick=FALSE, las=2, cex.axis=label.cex)
         abline(h=1:nseries, col="white")
         box()
-        par(op)
     }
-    res <- list(res.cor, res.pval, overall.cor, segavg.cor, seg.flags, bins)
-    names(res) <- c("spearman.rho", "p.val", "overall", "avg.seg.rho",
-         "flags", "bins")
-    res
+
+    list(spearman.rho = res.cor, p.val = res.pval, overall = overall.cor,
+         avg.seg.rho = segavg.cor, flags = seg.flags, bins = bins)
 }

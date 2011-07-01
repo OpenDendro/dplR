ccb1a2cc63fe45e98a0381a19c38ea7385982c17
Mikko Korpela <mvkorpel@iki.fi>
2011-07-01 16:03:13 +0000

    Large parts rewritten, 'glk(ca533)' example in glk.Rd runs about 100 times faster
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/glk.R
+++ R/glk.R
@@ -1,29 +1,28 @@
 glk <- function(x) {
-    count.common <- function(x) {
-        if (any(is.na(x))) 0
-        else 1
-    }
     n <- dim(x)[2]
     G <- matrix(NA, nrow = n, ncol = n)
     rownames(G) <- colnames(G) <- colnames(x)
     for (i in inc(1, n - 1)) {
         col1 <- x[, i]
-        for (k in (i + 1):n) {
-            col2 <- x[, k]
-            set <- cbind(col1, col2)
-            ## check if common interval is longer than 3 years
-            sum.common <- sum(apply(set, MARGIN = 1, count.common))
-            if (sum.common < 3) {
-                G[i, k] <- NA
-            } else {
-                set <- na.contiguous(set)
-                m <- dim(set)[1]
-                dif1 <- dif2 <- numeric(m - 1)
-                for (j in 2:m) {
-                    dif1[j - 1] <- sign(set[j, 1] - set[(j - 1), 1])/2
-                    dif2[j - 1] <- sign(set[j, 2] - set[(j - 1), 2])/2
+        not.na.1 <- which(!is.na(col1))
+        if (length(not.na.1) >= 3) {
+            for (k in (i + 1):n) {
+                col2 <- x[, k]
+                not.na.2 <- which(!is.na(col2))
+                ## check if common interval is longer than 3 years
+                not.na.both <- sort(intersect(not.na.1, not.na.2))
+                m <- length(not.na.both)
+                if (m >= 3) {
+                    if (not.na.both[m] - not.na.both[1] + 1 != m) {
+                        ## Should not happen; missing value marker is zero
+                        warning(gettextf("Intersection of series %d and %d is not contiguous. NA returned.",
+                                         i, k))
+                    } else {
+                        dif1 <- sign(diff(col1[not.na.both]))
+                        dif2 <- sign(diff(col2[not.na.both]))
+                        G[i, k] <- sum(abs(dif1 + dif2)) / (2 * m - 2)
+                    }
                 }
-                G[i, k] <- (1/(m - 1))*sum(abs(dif1 + dif2))
             }
         }
     }

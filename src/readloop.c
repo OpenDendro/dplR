69d72399cff6a5bfdb6abeba42c85647fec22ab9
Mikko Korpela <mvkorpel@iki.fi>
2011-11-22 11:52:18 +0000

    Cosmetic improvement
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@350 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/readloop.c
+++ src/readloop.c
@@ -1,6 +1,6 @@
 #include <R.h>
 
-/* A function to speed up the heaviest part of read.rwl.R */
+/* A function to speed up the heaviest part of read.tucson.R */
 /* Written by Mikko Korpela */
 void readloop(int *series_index, int *decade,
 	      int *x, int *x_nrow_p, int *x_ncol_p, int *min_year_p,
@@ -16,15 +16,15 @@ void readloop(int *series_index, int *decade,
     int rw_ncol = *rw_ncol_p;
 
     /* Convert between input and output formats */
-    for(i=0; i<x_nrow; i++){
+    for(i = 0; i < x_nrow; i++){
 	this_decade = decade[i];
 	yr_idx = this_decade - min_year;
 	this_series = series_index[i] - 1;
-	rw_idx = this_series*rw_nrow + yr_idx;
+	rw_idx = this_series * rw_nrow + yr_idx;
 	x_idx = i;
 	last_valid = this_decade - 1;
-	for(j=0; j<x_ncol; j++){
-	    this_val=x[x_idx];
+	for(j = 0; j < x_ncol; j++){
+	    this_val = x[x_idx];
 	    if(this_val == NA_INTEGER)
 		break;
 	    rw_mat[rw_idx++] = this_val;
@@ -40,9 +40,9 @@ void readloop(int *series_index, int *decade,
 		last_yr[this_series] = last_valid;
 	}
     }
-    for(i=0; i<rw_ncol; i++){
+    for(i = 0; i < rw_ncol; i++){
 	last_yr[i]--; /* (Assumed) stop marker is not real data */
-	stop_marker = rw_mat[i*rw_nrow+last_yr[i]-min_year+1];
+	stop_marker = rw_mat[i * rw_nrow + last_yr[i] - min_year + 1];
 	if(stop_marker == 999.0f)
 	    prec_rproc[i] = 100;
 	else if(stop_marker == -9999.0f)

91d1613a690d099384d5faacb180df1e44981c02
Filipe Campelo <fcampelo@ci.uc.pt>
2012-07-02 17:02:23 +0000

    common.interval function
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@635 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/common.interval.R
+++ R/common.interval.R
@@ -1,120 +1,96 @@
-## optimum.interval and common.interval functions
-## the optimum.interval function was deleted, now this code goes inside the common.interval function
-common.interval <- function(rwl, type=c("maximum", "optimum", "common"),
-                            make.plot=FALSE) {
-    if (!is.data.frame(rwl)) {
+common.interval = function(rwl, type=c("series", "years", "both"), make.plot=TRUE){
+
+if (!is.data.frame(rwl)) {
         stop("'rwl' must be a data.frame")
     }
+
     if (!all(vapply(rwl, is.numeric, FALSE, USE.NAMES=FALSE))) {
         stop("'rwl' must have numeric columns")
     }
+
     check.flags(make.plot)
-    ## "optimum" -> series and span
-    ## "maximum" -> span
-    ## "common"  -> series
-    type2 <- match.arg(type,c("maximum", "optimum", "common"))
-    rwl.orig <- rwl
-    ## to remove series without overlap
-    ## Notes from Mikko:
-    ## - How is this operation and "overlap" defined here?
-    ## - Let's imagine 'rwl' with two series: "A" spanning years
-    ##   1900-1920 and "B" years 1921-1940. In this case, the
-    ##   following would remove "A" and keep "B". The series don't
-    ##   overlap, and one of the two is (pretty arbitrarily)
-    ##   removed. Is this what we want?
-    series.range <- vapply(rwl, yr.range, numeric(2),
+	type2 <- match.arg(type,c("series", "years", "both"))
+    
+
+# rm.short is a function to remove short series and keep the series with overlap
+rm.short = function (rwl, flag=FALSE){
+n <- 0
+rwl <- rwl[,!apply(rwl, 2, function(x) all(is.na(x)))]
+series.range <- vapply(rwl, yr.range, numeric(2),
                            yr = as.numeric(row.names(rwl)))
-    dim(series.range) <- c(2, length(rwl))
-    first.year <- series.range[1, ]
-    to.keep <- series.range[2, ] > max(first.year, na.rm=TRUE)
-    to.keep[is.na(to.keep)] <- FALSE
-    rwl <- rwl[to.keep]
-    series.range <- series.range[, to.keep, drop=FALSE]
+   
+span.order <- order(series.range[2, ] - series.range[1, ])
+    to.keep <- rep(TRUE, length(span.order))
+      
+    rwl.output <- rwl
 
-    if (type2 == "common") {
-        rwl.output <- na.omit(rwl)
-    } else if (type2 == "optimum") {
-        ## Notes from Mikko:
-        ## - What function does this aim to optimize? Manual says it
-        ##   maximizes both the common time span and the number of
-        ##   series. This is ambiguous / impossible.
-        ## - Is the desired solution reached?
-		### The optimum.interval function gives the interval with the highest number of years (span * no of series)
-		### in some situations the "optimum" and the "maximum" options give the same
-        ### rwl.output <- optimum.interval(rwl)
-		rwl.output <- na.omit(rwl)
-		output <- ncol(rwl.output) * nrow(rwl.output)
-		## Testing for zero cols circumvents a possible bug in
-		## is.na.data.frame(). Checked with R Under development (unstable)
-		## (2012-06-19 r59583)
-		if (ncol(rwl) > 0) {
-			tmp <- rowSums(!is.na(rwl))
-		} else {
-        tmp <- rep(0, nrow(rwl))
-		}
-		yrs <- as.numeric(row.names(rwl))
-		for (i in dec(max(tmp), 2)) { ## Mikko: dec() forces a decreasing sequence
-			common.range <- range(yrs[tmp >= i])
-			rwl.common <- subset(rwl,
-								yrs >= common.range[1] & yrs <= common.range[2])
-			if (i * nrow(rwl.common) < output) {
-				break
-			}
-			## Notes from Mikko, about the following 2 lines of code:
-			## - first  line removes columns with maximum NA count (all NAs)
-			## - second line removes columns with a non-zero NA count
-			## Questions:
-			## - What is the point of the first line? Seems that the
-			##   operation is included in the operation defined on the
-			##   second line.
-			## - Is this really what we want? (i.e. remove columns with NAs)
-			## - Why such a complicated expression on the first line? It
-			##   seems that there is no need to call mean().
-			##FC yes you are completly right
-			##FC rwl.common <- rwl.common[!is.na(apply(rwl.common, 2, mean, na.rm=TRUE))]
-			##FC rwl.common <- as.data.frame(t(na.omit(t(rwl.common))))
-			rwl.common <- rwl.common[, !apply(rwl.common,2, function(x) any(is.na(x)))] ##FC
-				opt <- ncol(rwl.common) * nrow(rwl.common)
-			if (opt > output) {
-				output <- opt
-				rwl.output <- rwl.common
-			}
-		}
-    } else if (type2 == "maximum") {
-        ## to sort series [span]
-        span.order <- order(series.range[2, ] - series.range[1, ])
-        to.keep <- rep(TRUE, length(span.order))
-        n <- 0
-        rwl.output <- rwl
-        ## Notes from Mikko:
-        ## - Why 2, not 1, in length(span.order) - 2?
-		### is 2 because two series are needed to have an overlap
-        ## - Manual says this maximizes the common time span. How is
-        ##   it defined?
-		### By sorting the series by length, and then removing one by one (see the code below)
-        ## - Is the desired solution reached?
-
-        for (i in seq_len(max(0, length(span.order) - 2))) {
-            to.keep[span.order[i]] <- FALSE
+#for (i in seq_len(max(0, length(span.order) - 2))) {
+        for (i in seq(0, max(0, length(span.order) - 2))) {
+if(i>0){
+to.keep[span.order[i]] <- FALSE
+}
             rwl.short <- rwl[to.keep]
+if (ncol(rwl.short)*nrow(rwl.short)<n) {
+#to break if it is not possible to improve the common interval
+break
+}
             rwl.short <- na.omit(rwl.short)
             n.years <- ncol(rwl.short) * nrow(rwl.short)
             ## to keep the rwl if has more years
             if (n.years > n) {
-                n <- n.years
+    n <- n.years
                 rwl.output <- rwl.short
+if (flag) break #to give the common interval with the highest sample depth for the case of common.interval(rwl, type="series")
             }
         }
-    }
+rwl.output
+}
 
-    ## Notes from Mikko:
-    ## - If we have a span of 1 year (very hypothetical...), should
-    ##   the corresponding line segment have a non-zero length? I
-    ##   think it would be zero with the present code. An instance of
-    ##   a classic problem...
-    if (make.plot) {
+###########
+rwl.orig <- rwl
+yrs <- as.numeric(row.names(rwl))
+output <- 0
+opt <- 0
+
+## to get sample depth
+if (ncol(rwl) > 0) {
+tmp <- rowSums(!is.na(rwl))
+} else {
+tmp <- rep(0, nrow(rwl)) # is this line necessary?
+}
+
+for (i in dec(max(tmp), 2)) { ## Mikko: dec() forces a decreasing sequence
+tmp[tmp>i]<-i
+common.range <- range(as.integer(names(tmp)[tmp%in%i]))
+rwl.common <- subset(rwl,
+yrs >= common.range[1] & yrs <= common.range[2])
+if (i*nrow(rwl.common)<output){
+break
+}
+if (type2=="series"){
+rwl.output <- rm.short(rwl.common, flag=T)
+break
+} else if (type2=="years"){
+rwl.common <- rm.short(rwl.common)
+opt <- ncol(rwl.common)*nrow(rwl.common)
+}else if (type2=="both"){
+rwl.common <- rwl.common[, !apply(rwl.common,2, function(x) any(is.na(x)))] 
+opt <- ncol(rwl.common)*nrow(rwl.common)
+}
+if(opt>output) {
+output <- opt
+rwl.output <- rwl.common
+}
+}
+
+if (make.plot) {
         ## original rwl
+series.range <- vapply(rwl.orig, yr.range, numeric(2),
+                           yr = as.numeric(row.names(rwl)))
+   #dim(series.range) <- c(2, length(rwl))
+    first.year <- series.range[1, ]
         yr <- as.numeric(row.names(rwl.orig))
+
         neworder <- order(first.year, decreasing = FALSE)
         segs <- rwl.orig[neworder]
         n.col <- ncol(segs)
@@ -135,8 +111,9 @@ common.interval <- function(rwl, type=c("maximum", "optimum", "common"),
             }
         }
 
-        sub.str1 <- paste('Original: ',ncol(rwl.orig),' series, ',nrow(rwl.orig),' years',sep='')
-        sub.str2 <- paste('Common Interval (type=',type2,'): ',ncol(rwl.output),' series, ',nrow(rwl.output),' years',sep='')
+
+        sub.str1 <- paste("Original: ",ncol(rwl.orig)," series, ",nrow(rwl.orig)," years",sep="")
+        sub.str2 <- paste("Common Interval (type='",type2,"'): ",ncol(rwl.output)," series x ",nrow(rwl.output)," years = ",ncol(rwl.output)*nrow(rwl.output)," years.", sep='')
         sub.str <- paste(sub.str1,'\n',sub.str2)
         op <- par(no.readonly = TRUE)
         on.exit(par(op))
@@ -154,5 +131,8 @@ common.interval <- function(rwl, type=c("maximum", "optimum", "common"),
         axis(3, at=range.output, labels=range.output, tcl=-0.25)
         box()
     }
-    rwl.output
+
+
+rwl.output
 }
+

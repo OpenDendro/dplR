627b725563e11c88eb712bf255f8fb9f0f2c24c1
Andy Bunn <andy.bunn@wwu.edu>
2014-01-11 06:29:53 +0000

    trying to track down error when R CMD check --as-cran dplR_1.5.8.tar.gz is called.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@718 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/tbrm.R
+++ R/tbrm.R
@@ -3,6 +3,6 @@
     y <- as.double(x[!is.na(x)])
     n <- as.integer(length(y))
     stopifnot(!is.na(n))
-    .C(dplR.tbrm,
-       y, n, as.double(C), result=NaN, NAOK=TRUE, DUP=FALSE)$result
+    .C(dplR.tbrm, y, n, as.double(C), result = double(1L), NAOK = TRUE,
+       DUP = FALSE)$result
 }

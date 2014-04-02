976baf97dd2971579459d1be11994ce1ad7b515a
Mikko Korpela <mvkorpel@iki.fi>
2014-04-02 12:36:30 +0000

    Optimizations
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@759 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/series.rho.R
+++ R/series.rho.R
@@ -1,16 +1,16 @@
-series.rho <- function(rwl,n=NULL,prewhiten=TRUE,biweight=TRUE){
-    nseries <- ncol(rwl)
-    rho.df <- data.frame(rho=rep(NA,nseries),p.val=rep(NA,nseries))
-    rownames(rho.df) <- colnames(rwl)
-    for(i in 1:nseries){
-      tmp <- normalize.xdate(rwl=rwl[,-i],series=rwl[,i],
-                             n=n,prewhiten=prewhiten,biweight=biweight)
-      tmp <- data.frame(series=tmp$series,master=tmp$master)
-      mask <- rowSums(is.na(tmp)) == 0
-      tmp2 <- cor.test(tmp$series[mask], tmp$master[mask],
-                      method = "spearman", alternative = "greater")
-      rho.df[i,1] <- tmp2$estimate
-      rho.df[i,2] <- tmp2$p.val
+series.rho <- function(rwl, n=NULL, prewhiten=TRUE, biweight=TRUE) {
+    nseries <- length(rwl)
+    rho <- numeric(nseries)
+    p.val <- numeric(nseries)
+    rwl.mat <- as.matrix(rwl)
+    for (i in seq_len(nseries)) {
+        tmp <- normalize.xdate(rwl=rwl.mat[, -i, drop=FALSE],
+                               series=rwl.mat[, i], n=n,
+                               prewhiten=prewhiten, biweight=biweight)
+        tmp2 <- cor.test(tmp[["series"]], tmp[["master"]],
+                         method = "spearman", alternative = "greater")
+        rho[i] <- tmp2[["estimate"]]
+        p.val[i] <- tmp2[["p.value"]]
     }
-    rho.df
+    data.frame(rho = rho, p.val = p.val, row.names = names(rwl))
 }

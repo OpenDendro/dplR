5bed7a61b518535ef5728585367b9c3bde33a147
Mikko Korpela <mvkorpel@iki.fi>
2014-04-09 14:51:35 +0000

    Changed default behavior: prompt before every removal
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@798 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- svnclean.sh
+++ svnclean.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # http://stackoverflow.com/questions/5474732/how-can-i-add-a-help-method-to-a-shell-script
-usage="$(basename "$0") [-h] [-l] [-p]
+usage="$(basename "$0") [-h] [-f] [-l]
 
 Remove files that are not controlled by svn,
 
 where:
     -h  show this help text
-    -p  prompt before every removal (default: force, i.e. never ask)
+    -f  don't ask questions (default: prompt before every removal)
     -l  only list the files to be removed"
 
 listfiles=0
-rmprompt=0
+rmprompt=1
 # http://stackoverflow.com/questions/4882349/parsing-shell-script-arguments
 while [[ $1 == -* ]]; do
     case "$1" in
 	-h) echo "$usage"; exit;;
 	-l) listfiles=1; shift;;
-	-p) rmprompt=1; shift;;
+	-f) rmprompt=0; shift;;
 	-*) echo "invalid option: $1" 1>&2; exit 1;;
     esac
 done

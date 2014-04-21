0b039969a4fe32e2fd180932bbe69a4e21e50375
Mikko Korpela <mvkorpel@iki.fi>
2014-04-21 19:34:45 +0000

    detrend() now has a 'return.info' argument like detrend.series()
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@829 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/detrend.R
+++ R/detrend.R
@@ -3,10 +3,12 @@
              method=c("Spline", "ModNegExp", "Mean", "Ar"),
              nyrs = NULL, f = 0.5, pos.slope = FALSE,
              constrain.modnegexp = c("never", "when.fail", "always"),
-             verbose=FALSE)
+             verbose = FALSE, return.info = FALSE)
 {
     stopifnot(identical(make.plot, TRUE) || identical(make.plot, FALSE),
-              identical(pos.slope, FALSE) || identical(pos.slope, TRUE))
+              identical(pos.slope, FALSE) || identical(pos.slope, TRUE),
+              identical(verbose, TRUE) || identical(verbose, FALSE),
+              identical(return.info, TRUE) || identical(return.info, FALSE))
     known.methods <- c("Spline", "ModNegExp", "Mean", "Ar")
     constrain2 <- match.arg(constrain.modnegexp)
     method2 <- match.arg(arg = method,
@@ -38,35 +40,58 @@
         out <- foreach::"%dopar%"(foreach::foreach(rwl.i=it.rwl,
                                                    .export=exportFun),
                               {
-                                  fits <- detrend.series(rwl.i, make.plot=FALSE,
-                                                         method=method2,
-                                                         nyrs=nyrs, f=f,
-                                                         pos.slope=pos.slope,
-                                                         constrain.modnegexp=
-                                                         constrain2,
-                                                         verbose=verbose)
-                                  if(is.data.frame(fits))
-                                      row.names(fits) <- rn
-                                  fits
+                                  names(rwl.i) <- rn
+                                  detrend.series(rwl.i, make.plot=FALSE,
+                                                 method=method2,
+                                                 nyrs=nyrs, f=f,
+                                                 pos.slope=pos.slope,
+                                                 constrain.modnegexp=
+                                                 constrain2,
+                                                 verbose=FALSE,
+                                                 return.info=return.info)
                               })
+
+        if (return.info) {
+            modelStats <- lapply(out, "[[", 2)
+            dataStats <- lapply(out, "[[", 3)
+            out <- lapply(out, "[[", 1)
+        }
     } else{
-        out <- list()
-        for(i in seq_len(ncol(rwl))){
+        n.series <- ncol(rwl)
+        out <- vector(mode = "list", length = n.series)
+        if (return.info) {
+            modelStats <- vector(mode = "list", length = n.series)
+            dataStats <- vector(mode = "list", length = n.series)
+        }
+        for (i in seq_len(n.series)) {
             fits <- detrend.series(rwl[[i]], y.name=y.name[i],
                                    make.plot=make.plot,
                                    method=method2, nyrs=nyrs, f=f,
                                    pos.slope=pos.slope,
                                    constrain.modnegexp=constrain2,
-                                   verbose=verbose)
-            if(is.data.frame(fits))
+                                   verbose=verbose, return.info=return.info)
+            if (return.info) {
+                modelStats[[i]] <- fits[[2]]
+                dataStats[[i]] <- fits[[3]]
+                fits <- fits[[1]]
+            }
+            if (is.data.frame(fits)) {
                 row.names(fits) <- rn
+            }
             out[[i]] <- fits
         }
     }
-    names(out) <- names(rwl)
+    series.names <- names(rwl)
+    names(out) <- series.names
     if(length(method2) == 1){
         out <- data.frame(out, row.names = rn)
         names(out) <- y.name
     }
-    out
+    if (return.info) {
+        names(modelStats) <- series.names
+        names(dataStats) <- series.names
+        list(series = out, model.info = modelStats, data.info = dataStats)
+    } else {
+        out
+    }
 }

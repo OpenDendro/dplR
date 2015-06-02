ea08cf3f6541ceae5e8274cf64a9f91e095a84e3
Mikko Korpela <mvkorpel@iki.fi>
2015-06-02 15:03:06 +0000

    In treeMean(), * Renamed first argument to 'x' (was already named so in the   \arguments section of the .Rd file). * Inputs are checked. * A warning is produced if there are missing tree IDs, which is   probably a rare occasion. The series with NA IDs are now   averaged. Is this desirable or not?  The resulting average may not   be very useful. Prior to my changes, the function would return a   completely NA-filled "average" for series with a missing tree ID. * Default 'ids' is NULL, means one core per tree (no averaging). * Row names of 'ids' (if any) are matched to column names of 'x'. * Small optimizations were made. * Code and examples were reformatted a bit. * Note: Should the return value inherit class "rwl"?
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/dplr/pkg/dplR@992 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/treeMean.R
+++ R/treeMean.R
@@ -1,14 +1,39 @@
-treeMean <- function(rwl,ids){
-  rwl2 <- as.matrix(rwl)
-  trees <- ids$tree
-  uTrees <- unique(trees)
-  res <- matrix(NA, nrow=nrow(rwl2), ncol=length(uTrees))
-  for (i in seq(along=uTrees)) {
-    res[,i] <- rowMeans(rwl2[,trees == uTrees[i], drop=FALSE], na.rm=TRUE)
-  }
-  res[is.nan(res)] <- NA
-  res <- as.data.frame(res)
-  colnames(res) <- uTrees
-  rownames(res) <- rownames(rwl)
-  res
+treeMean <- function(x, ids = NULL) {
+    ## If 'ids' is NULL then assume one core per tree (no averaging)
+    if (is.null(ids)) {
+        res <- as.data.frame(x)
+        names(res) <- seq_len(length(res))
+        return(res)
+    }
+    x2 <- as.matrix(x)
+    if (!is.data.frame(ids) || !("tree" %in% names(ids))) {
+        stop("'ids' must be a data.frame with column 'tree'")
+    }
+    colnames.x <- colnames(x2)
+    trees <- as.matrix(ids["tree"])
+    rownames.ids <- rownames(trees)
+    ## If all column names in 'x' are present in the set of row
+    ## names in 'ids', arrange 'ids' to matching order
+    if (!is.null(rownames.ids) && !is.null(colnames.x) &&
+        anyDuplicated(colnames.x) == 0 &&
+        all(colnames.x %in% rownames.ids)) {
+        trees <- trees[colnames.x, ]
+    } else if (length(trees) == ncol(x2)) {
+        trees <- as.vector(trees)
+    } else {
+        stop("dimension problem: ", "'ncol(x)' != 'nrow(ids)'")
+    }
+    uTrees <- unique(trees)
+    if (any(is.na(uTrees))) {
+        warning("series with missing tree IDs, will be averaged")
+    }
+    matches <- match(trees, uTrees)
+    res <- matrix(NA_real_, nrow=nrow(x2), ncol=length(uTrees))
+    for (i in seq_along(uTrees)) {
+        res[, i] <- rowMeans(x2[, matches == i, drop=FALSE], na.rm=TRUE)
+    }
+    res[is.nan(res)] <- NA_real_
+    res <- as.data.frame(res, row.names = rownames(x2))
+    names(res) <- uTrees
+    res
 }

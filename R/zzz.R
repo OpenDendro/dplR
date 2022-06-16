2df573dde7a2513c944c2807831590f5416524e6
Andy Bunn <andy.bunn@wwu.edu>
2022-06-16 14:33:07 -0700

    Adding a chron function that returns CIs.

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,7 @@
   packageStartupMessage("This is dplR version ", 
                         packageVersion("dplR"),
                         ".\n",
+                        "dplR is part of openDendro https://opendendro.org",
+                        ".\n",
                         "New users can visit https://opendendro.github.io/dplR-workshop/ to get started.")
 }

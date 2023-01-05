bd29742ac5b8c923ddf015342c3bf2c471e9f4a4
Andy Bunn <andy.bunn@wwu.edu>
2023-01-05 15:28:18 -0800

    Added a stop and error message to csv2rwl to prevent duplicate series IDs a la read.tucson.

--- R/csv2rwl.R
+++ R/csv2rwl.R
@@ -1,8 +1,18 @@
 `csv2rwl` <- function(fname,...)
 {
-  dat <- read.table(fname, header=TRUE, sep=",",...)
-  rownames(dat) <- as.character(dat[,1])
-  dat <- dat[,-1]
-  class(dat) <- c("rwl","data.frame")
-  dat
+  # check for unique names
+  nameCheck <- scan(fname,nlines=1,what = "char",sep=",")
+  if(any(duplicated(nameCheck))){
+    warn.fmt <- gettext("Duplicated series ID detected: %s",domain="R-dplR")
+    ids.dup <- paste(nameCheck[which(duplicated(nameCheck))],
+                     collapse = "; ")
+    stop(sprintf(warn.fmt, ids.dup), domain=NA) 
+  }
+  else{
+    dat <- read.table(fname, header=TRUE, sep=",",check.names=TRUE,...)
+    rownames(dat) <- as.character(dat[,1])
+    dat <- dat[,-1]
+    class(dat) <- c("rwl","data.frame")
+    return(dat)
+  }
 }

77c3372e2b2908ce640b72b0a455ad086a18ad41
AndyBunn <bunna@wwu.edu>
2024-02-23 14:53:27 -0800

    Removing plyr as an import. that was NOT needed and caused bloat. Also cleaning and adding to rwl.report generally.

--- R/rwl.report.R
+++ R/rwl.report.R
@@ -2,10 +2,17 @@ rwl.report <- function(rwl, small.thresh = NA, big.thresh = NA){
   oldw <- getOption("warn")
   options(warn = -1)
   
+  # make sure input is class(rwl)
+  if (!inherits(rwl, "rwl")) {
+    stop('use only with "rwl" objects')
+  }
+  
   res <- list()
   res$small.thresh <- small.thresh
   res$big.thresh <- big.thresh
-  # get a summary
+  
+  
+  # start with a summary
   tmp.sum <- summary.rwl(rwl)
   res$nseries <- ncol(rwl)
   res$n <- length(rwl[!is.na(rwl)])
@@ -14,9 +21,7 @@ rwl.report <- function(rwl, small.thresh = NA, big.thresh = NA){
   res$yr1 <- max(tmp.sum$last)
   res$ar1bar <- mean(tmp.sum$ar1)
   res$ar1bar.sd <- sd(tmp.sum$ar1)
-  # interseries
-  res$interrbar <- mean(interseries.cor(rwl)[,1])
-  res$interrbar.sd <- sd(interseries.cor(rwl)[,1])
+  
   
   # missing rings
   zedsLogical <- rwl == 0
@@ -31,12 +36,59 @@ rwl.report <- function(rwl, small.thresh = NA, big.thresh = NA){
   samps <- rowSums(!is.na(rwl))
   pctSeriesZero <- rowSums(zedsLogical,na.rm = TRUE)/samps
   res$allZeroYears <- which(pctSeriesZero==1)
-  print(res$allZeroYears)
+  
+  # Any places with >1 consecutive zeros?
+  # Find runs of consecutive zeros
+  consecutiveZedsVec <- function(x){
+    # Extract lengths of runs and values using the ever confusing rle()
+    runs <- rle(x == 0)
+    run_lengths <- runs$lengths
+    run_values <- runs$values
+    
+    # Identify positions where there are >1 consecutive zeros
+    consecutive_zeros_indices <- which(run_values & run_lengths > 1)
+    
+    # Create a logical vector of length x indicating where there are >1 consecutive zeros
+    consecutive_zeros_logical <- logical(length(x))
+    for (i in consecutive_zeros_indices) {
+      consecutive_zeros_logical[(sum(run_lengths[1:(i-1)]) + 1):(sum(run_lengths[1:i]))] <- TRUE
+    }
+    consecutive_zeros_logical
+  }
+  
+  consecutiveZedsLogical <- apply(rwl,2,consecutiveZedsVec)
+  rownames(consecutiveZedsLogical) <- time(rwl)
+  # make a list where every series is an element
+  consecutiveZedsLogicalList <- apply(consecutiveZedsLogical,2,which)
+  # get years from names instead of indices
+  consecutiveZedsLogicalList <- sapply(consecutiveZedsLogicalList, 
+                                       function(x) {as.numeric(names(x))})
+  # drop series without consec zeroes 
+  mask <- lapply(consecutiveZedsLogicalList,length)>0
+  consecutiveZedsLogicalList <- consecutiveZedsLogicalList[mask]
+  # clean up for output
+  if(length(consecutiveZedsLogicalList)<1) res$consecutiveZeds <- numeric(0)
+  else res$consecutiveZeds <- consecutiveZedsLogicalList
+  
+  
+  # check overlap of all series
+  
+  # interseries correlation
+  res$interrbar <- mean(interseries.cor(rwl)[,1])
+  res$interrbar.sd <- sd(interseries.cor(rwl)[,1])
+  
+  
   # internal NA
-  internalNAs <- alply(rwl, 2, find.internal.na) # like apply but forces a list
+  # this used alply from plyr. cutting it out. feb 2024
+  # internalNAs <- alply(rwl, 2, find.internal.na) # like apply but forces a list
+  # names(internalNAs) <- names(rwl)
+  # internalNAs <- sapply(internalNAs, function(x) {as.numeric(rownames(rwl)[x])} )
+  # internalNAs <- internalNAs[lapply(internalNAs,length)>0]
+  internalNAs <- as.list(apply(rwl, 2, find.internal.na))
   names(internalNAs) <- names(rwl)
-  internalNAs <- sapply(internalNAs, function(x) {as.numeric(rownames(rwl)[x])} )
+  internalNAs <- sapply(internalNAs, function(x) {as.numeric(names(x))} )
   internalNAs <- internalNAs[lapply(internalNAs,length)>0]
+  
   if(length(internalNAs)<1) res$internalNAs <- numeric(0)
   else res$internalNAs <- internalNAs
 
@@ -70,7 +122,7 @@ rwl.report <- function(rwl, small.thresh = NA, big.thresh = NA){
 print.rwl.report <- function(x, ...){
   cat("Number of dated series:",x$nseries,"\n")
   cat("Number of measurements:",x$n,"\n")
-  cat("Number of missing rings: ", x$nzeros, 
+  cat("Number of absent rings: ", x$nzeros, 
       " (", round(x$nzeros/x$n * 100, 3),"%)\n",sep="")    
   cat("Avg series length:",x$segbar,"\n")
   cat("Range: ", x$yr1 - x$yr0 + 1, "\n")
@@ -80,10 +132,10 @@ print.rwl.report <- function(x, ...){
   cat("Mean (Std dev) AR1: ",x$ar1bar, " (", 
       x$ar1bar.sd,")\n",sep="")
   cat("-------------\n")
-  cat("Years with where all rings are missing\n")
+  cat("Years with where all rings are absent\n")
   if(length(x$allZeroYears)==0) cat("    None \n")
   else{
-    cat("Warning: Having years with all zeros is atypical. \n It can break dplR functions (e.g. ssf) with div0 issues resulting in Inf and NaN \n")
+    cat("Warning: Having years with all zeros is atypical (but not unheard of). \n It can break dplR functions (e.g., ssf) with div0 issues resulting in Inf and NaN \n")
     cat(x$allZeroYears,"\n")
   }
   
@@ -95,6 +147,17 @@ print.rwl.report <- function(x, ...){
       tmp = x$zeros[[i]]
       if(length(tmp)==0) next()
       cat("    Series", names(x$zeros)[i],"--",tmp,"\n",  
+          sep = " ")
+    }
+  }
+  cat("-------------\n")
+  cat("Years with more than one consecutive absent rings listed by series \n")
+  if(length(x$consecutiveZeds)==0) cat("    None \n")
+  else{
+    for(i in 1:length(x$consecutiveZeds)){
+      tmp = x$consecutiveZeds[[i]]
+      if(length(tmp)==0) next()
+      cat("    Series", names(x$consecutiveZeds)[i],"--",tmp,"\n",  
           sep = " ")
     }
   }
@@ -136,5 +199,5 @@ print.rwl.report <- function(x, ...){
       }
     }
   }
-  invisible(x)
+  #invisible(x)
 }
